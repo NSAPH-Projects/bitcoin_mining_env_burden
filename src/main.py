f04b7d04414d1e7d8c7d5362fa7f373ba641575e
Jonathan Gilmour <jonatgilmour@gmail.com>
2024-09-05 11:07:12 -0400

    CLI support for inmap processing and requires specification of save path

--- src/main.py
+++ src/main.py
@@ -8,14 +8,23 @@ from args import parser
 sys.path.append(os.path.abspath(os.path.join('../src')))
 
 from emission_calculator import complete_dataset
+from inmap_processing import inmap_data
 
 def main(args_list=None):
     if args_list is None:
         args_list = sys.argv[1:]
     args = parser.parse_args(args_list)
-    if args.save_path:
+    if args.emissions:
         complete_dataset(base_dir=args.base_dir, save_path=args.save_path)
     else:
-        complete_dataset(base_dir=args.base_dir)
+        #check to make sure that input data is the complete dataset
+        path = args.base_dir + 'df_complete_2022_2023.csv'
+        if not os.path.isfile(path):
+            raise ValueError('Cannot find a file named df_complete_2022_2023.csv in the base_dir specified.')
+        #check to make sure that save_path is a directory
+        if not os.path.isdir(args.save_path):
+            raise ValueError('Specified save_path is not a valid directory. Please specify a directory in which to save the output.')
+        inmap_data(input_data=path, output_data_path=args.save_path + '/emissions_2022_2023.shp')
+
 if __name__ == '__main__':
     main()
